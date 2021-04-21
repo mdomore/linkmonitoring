36a957af6ce4613314741353ee272b00ba23e3f7
Michael Moreliere <michael.moreliere@gmail.com>
2021-04-21 18:26:37 +0200

    Try working with pandas Dataframes

--- main.py
+++ main.py
@@ -4,70 +4,29 @@ import json
 import psycopg2
 import time
 import re
-import logging
-from timeloop import Timeloop
-from datetime import timedelta
+import sys
+import os
+import pandas as pd
+from io import StringIO
 
 # Variables and Constants
-long_logins = []
-link_login = []
-connected_logins = []
-db_logins = []
-result = []
-
-short_logins = set()
 
 OID_CISCO_1 = '.1.3.6.1.4.1.9.9.786.1.2.1.1.6.1.1'
 OID_CISCO_22 = '.1.3.6.1.4.1.9.9.786.1.1.1.1.22'
 OID_CISCO_23 = '.1.3.6.1.4.1.9.9.786.1.1.1.1.23'
 OID_CISCO_24 = '.1.3.6.1.4.1.9.9.786.1.1.1.1.24'
 
-tl = Timeloop()
-
 re_new_login = re.compile('^\w{2}\d{6,8}-\d-L\d{2}')
 
-select_all_query = """SELECT * FROM old_login;"""
+dict_connected = {'username': [], 'last_down': [], 'last_up': [], 'status': [], 'subtype': [], 'login_status': []}
 
-update_round_timestamp = """
-                        UPDATE all_purpose SET value = %s WHERE title = %s;
-                        """
-
-upsert_up = """
-            INSERT INTO old_login (username, last_up, status, subtype, login_status) \
-            VALUES (%s, %s, %s, %s, %s) \
-            ON CONFLICT (username) \
-            DO UPDATE SET last_up = %s, status = %s, subtype = %s, login_status = %s \
-            WHERE old_login.username = %s;
-            """
-
-upsert_down = """
-            INSERT INTO old_login (username, last_down, status, subtype, login_status) \
-            VALUES (%s, %s, %s, %s, %s) \
-            ON CONFLICT (username) \
-            DO UPDATE SET last_down = %s, status = %s, subtype = %s, login_status = %s \
-            WHERE old_login.username = %s;
-            """
-
-
-upsert_login_status_up = """
-                        INSERT INTO old_login (username, login_status, last_up, subtype) \
-                        VALUES (%s, %s, %s, %s) \
-                        ON CONFLICT (username) \
-                        DO UPDATE SET login_status = %s, last_up = %s, subtype = %s \
-                        WHERE old_login.username = %s;
-                        """
-
-upsert_login_status_down = """
-                        INSERT INTO old_login (username, login_status, last_down, subtype) \
-                        VALUES (%s, %s, %s, %s) \
-                        ON CONFLICT (username) \
-                        DO UPDATE SET login_status = %s, last_down = %s, subtype = %s \
-                        WHERE old_login.username = %s;
-                        """
-
-update_link_status = """
-                     UPDATE old_login SET status = %s WHERE username = %s;
-                     """
+# Connection parameters
+param_dic = {
+    "host": "172.22.0.11",
+    "database": "login_status",
+    "user": "postgres",
+    "password": "MilKa;MeuH"
+}
 
 
 def redbackSNMPWalk(data, ip, version, community):
@@ -95,13 +54,12 @@ def redbackSNMPWalk(data, ip, version, community):
             vlan = vlan
         else:
             vlan = loginClassify(data, realm)
-        long_logins.append(
-            {
-                'user': user,
-                'type': vlan,
-                'status': True
-            }
-        )
+        dict_connected['username'].append(user)
+        dict_connected['last_down'].append(0)
+        dict_connected['last_up'].append(psycopg2.TimestampFromTicks(time.time() // 1))
+        dict_connected['status'].append(True)
+        dict_connected['subtype'].append(vlan)
+        dict_connected['login_status'].append(True)
         i += 1
 
 
@@ -189,14 +147,20 @@ def ciscoSNMPGet(data, h, ip, version, community):
             realm = (reply[1].decode("utf-8").split('.', 1)[1])
             user = (reply[2].decode("utf-8"))
             if type != 0:
-                long_logins.append(
-                    (user, (psycopg2.TimestampFromTicks(time.time() // 1)), True, type, True)
-                )
+                dict_connected['username'].append(user)
+                dict_connected['last_down'].append(0)
+                dict_connected['last_up'].append(psycopg2.TimestampFromTicks(time.time() // 1))
+                dict_connected['status'].append(True)
+                dict_connected['subtype'].append(type)
+                dict_connected['login_status'].append(True)
             else:
-                type = loginClassify(data, realm)
-                long_logins.append(
-                    (user, (psycopg2.TimestampFromTicks(time.time() // 1)), True, type, True)
-                )
+                vlan = loginClassify(data, realm)
+                dict_connected['username'].append(user)
+                dict_connected['last_down'].append(0)
+                dict_connected['last_up'].append(psycopg2.TimestampFromTicks(time.time() // 1))
+                dict_connected['status'].append(True)
+                dict_connected['subtype'].append(vlan)
+                dict_connected['login_status'].append(True)
         except IndexError:
             print(reply)
             pass
@@ -220,35 +184,70 @@ def loginClassify(data, realm):
                 pass
 
 
-def push_login_to_db(cur, login, state, vlan):
-    if state:
-        cur.execute(upsert_login_status_up, (login, True, psycopg2.TimestampFromTicks(time.time() // 1), vlan,
-                                             True, psycopg2.TimestampFromTicks(time.time() // 1), vlan, login))
-    elif not state:
-        cur.execute(upsert_login_status_down, (login, False, psycopg2.TimestampFromTicks(time.time() // 1), vlan,
-                                               False, psycopg2.TimestampFromTicks(time.time() // 1), vlan, login))
-
-
-def sql_conn():
-
-    conn = psycopg2.connect(
-        host="172.22.0.11",
-        database="login_status",
-        user="postgres",
-        password="MilKa;MeuH")
+def connect(params_dic):
+    """ Connect to the PostgreSQL database server """
+    conn = 0
+    try:
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params_dic)
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        sys.exit(1)
+    print("Connection successful")
     return conn
 
 
-# Launch the script every X seconds
-@tl.job(interval=timedelta(seconds=300))
-def main():
-    logging.info("starting link monitoring")
-    '''
-    Pushing collected data in DB
-    '''
-    conn = sql_conn()
-    cur = conn.cursor()
+def copy_from_file(conn, df, table):
+    """
+    Here we are going save the dataframe on disk as
+    a csv file, load the csv file
+    and use copy_from() to copy it to the table
+    """
+    # Save the dataframe to disk
+    tmp_df = "./tmp_dataframe.csv"
+    df.to_csv(tmp_df, index=False, header=False)
+    f = open(tmp_df, 'r')
+    cursor = conn.cursor()
+    try:
+        cursor.copy_from(f, table, sep=",")
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        os.remove(tmp_df)
+        print("Error: %s" % error)
+        conn.rollback()
+        cursor.close()
+        return 1
+    print("copy_from_file() done")
+    cursor.close()
+    # os.remove(tmp_df)
 
+
+def copy_from_stringio(conn, df, table):
+    """
+    Here we are going save the dataframe in memory
+    and use copy_from() to copy it to the table
+    """
+    # save dataframe to an in memory buffer
+    buffer = StringIO()
+    df.to_csv(buffer, index=False, header=False)
+    buffer.seek(0)
+
+    cursor = conn.cursor()
+    try:
+        cursor.copy_from(buffer, table, sep=",")
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+
+        print("Error: %s" % error)
+        conn.rollback()
+        cursor.close()
+        return 1
+    print("copy_from_stringio() done")
+    cursor.close()
+
+
+def main():
     with open("config.json") as json_data_file:
         data = json.load(json_data_file)
         for h in data['hosts']:
@@ -265,63 +264,33 @@ def main():
             if brand == 'cisco':
                 ciscoTotalSub(data, h, ip, data['snmp']['version'], data['snmp']['community'])
                 ciscoSNMPGet(data, h, ip, data['snmp']['version'], data['snmp']['community'])
-        '''
-        push time of this round in db
-        '''
-        cur.execute(update_round_timestamp, (psycopg2.TimestampFromTicks(time.time() // 1), 'last_round'))
-        '''
-        Update link status for long_logins in new format <IP.........-.-L--@realm>
-        '''
-        print(long_logins)
-        breakpoint()
-        # print('SQL connected logins')
-        # print("--- %s seconds ---" % (time.time() - start_time))
-        # for long_login in long_logins:
-        #     link_login.append(long_login['user'][:12])
-        #     short_logins.add(long_login['user'])
-        #     if 'factory' in long_login['user']:
-        #         pass
-        #     else:
-        #         cur.mogrify(upsert_up, tuple)
-        #         breakpoint()
-        #         push_login_to_db(cur, long_login['user'], True, long_login['type'])
-        #
-        # print('SQL select all')
-        # print("--- %s seconds ---" % (time.time() - start_time))
-        # cur.execute(select_all_query)
-        # select_all = cur.fetchall()
-        # for db_login, db_date_down, db_date_up, db_link_status, db_type, db_login_status in select_all:
-        #     db_logins.append(
-        #         {
-        #             'user': db_login,
-        #             'type': db_type,
-        #             'status': db_login_status
-        #         }
-        #     )
-        #
-        # print('SQL db_login')
-        # print("--- %s seconds ---" % (time.time() - start_time))
-        # for db_login in db_logins:
-        #     if 'factory' in db_login['user']:
-        #         pass
-        #     else:
-        #         if db_login['user'] not in short_logins:
-        #             push_login_to_db(cur, db_login['user'], False, db_login['type'])
-        #         if db_login['user'][:12] not in link_login:
-        #             cur.execute(update_link_status, (False, db_login['user']))
-        #         else:
-        #             cur.execute(update_link_status, (True, db_login['user']))
-        # '''
-        # SQL commit
-        # '''
-        conn.commit()
-        conn.close()
-    print("PostgreSQL connection pool is closed")
-    logging.info("stopping link monitoring")
+
+    df_connected = pd.DataFrame(data=dict_connected)
+
+    conn = connect(param_dic)
+    sql = """SELECT * FROM old_login;"""
+    df_db = pd.read_sql_query(sql, conn)
+    conn.close()
+
+    frames = [df_connected, df_db]
+    result = pd.concat(frames)
+
+    print(df_db)
+    print(df_connected)
+
+
+
+    # result.to_csv(r'export_dataframe.csv', index=False, header=True)
+
+    #print(result)
+
+    # conn = connect(param_dic)  # connect to the database
+    # # copy_from_stringio(conn, df, 'old_login')  # copy the dataframe to SQL
+    # copy_from_file(conn, df, 'old_login')  # copy the dataframe to SQL
+    # conn.close()  # close the connection
 
 
 if __name__ == "__main__":
     start_time = time.time()
-    #tl.start(block=True)
     main()
     print("--- %s seconds ---" % (time.time() - start_time))
